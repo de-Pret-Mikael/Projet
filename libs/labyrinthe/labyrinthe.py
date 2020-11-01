a646f2d4c5282e3ac44cd1e9cd962605cd6d471c
cyrilGrandjean <64021617+cyrilGrandjean@users.noreply.github.com>
2020-11-01 17:38:09 +0100

    modification de la class labyrinthe

--- libs/labyrinthe/labyrinthe.py
+++ libs/labyrinthe/labyrinthe.py
@@ -25,23 +25,23 @@ class cell:
     def id(self):
         return self.__id
 
-    def cellAdj(self,xMax,yMax):
+    def cellAdj(self, xMax, yMax):
         """
         :param xMax: valeur max de x
         :param yMax: valeur max de y
-        :return envoie un dictionnaire
+        :return envoie un dictionnaire de dictionnnaire composé des cellule adjacente
         """
         dic = {}
-        xMax = 2*xMax
-        yMax = 2*yMax
-        if self.y-1 >= 0:
-            dic["top"] = self.y-1
-        if self.x+1 <= xMax:
-            dic["right"] = self.x+1
-        if self.y+1 <= yMax:
-            dic["down"] = self.y+1
-        if self.x-1 >= 0:
-            dic["left"] = self.x-1
+        xMax = 2 * xMax
+        yMax = 2 * yMax
+        if self.y - 1 >= 0:
+            dic["top"] = {"x": self.x, "y": self.y - 1}
+        if self.x + 1 <= xMax:
+            dic["right"] = {"x": self.x + 1, "y": self.y}
+        if self.y + 1 <= yMax:
+            dic["down"] = {"x": self.x, "y": self.y + 1}
+        if self.x - 1 >= 0:
+            dic["left"] = {"x": self.x - 1, "y": self.y}
         return dic
 
 class labyrinthe:
@@ -59,6 +59,9 @@ class labyrinthe:
     @property
     def width(self):
         return self.__width
+
+    def get_cell(self, x, y):
+        return self.laby[y][x]
 
     def buildGrid(self):
         """"""
@@ -82,28 +85,28 @@ class labyrinthe:
                 rand = random.randrange(0, len(self.wall))
             else:
                 break
-            coord = list(map(lambda y:int(y), self.wall[rand].split(",")))
+            coord = list(map(lambda y: int(y), self.wall[rand].split(",")))
             del self.wall[rand]
-            cell = self.laby[coord[1]][coord[0]]
-            dic = cell.cellAdj(self.width,self.height)
+            cell = self.get_cell(coord[0], coord[1])
+            dic = cell.cellAdj(self.width, self.height)
             if not coord[0] % 2:
-                vRigth = self.laby[coord[1]][dic["right"]].numb
-                vLeft = self.laby[coord[1]][dic["left"]].numb
-                if not (vRigth==vLeft):
+                vRigth = self.get_cell(**dic["right"]).numb
+                vLeft = self.get_cell(**dic["left"]).numb
+                if not (vRigth == vLeft):
                     cell.wall = False
                     if (vRigth > vLeft):
-                        self.newVal(vRigth,vLeft)
+                        self.newVal(vRigth, vLeft)
                     else:
-                        self.newVal(vLeft,vRigth)
+                        self.newVal(vLeft, vRigth)
             if not coord[1] % 2:
-                vDown = self.laby[dic["down"]][coord[0]].numb
-                vTop =self.laby[dic["top"]][coord[0]].numb
+                vDown = self.get_cell(**dic["down"]).numb
+                vTop = self.get_cell(**dic["top"]).numb
                 if not (vDown == vTop):
                     cell.wall = False
                     if (vDown > vTop):
-                        self.newVal(vDown,vTop)
+                        self.newVal(vDown, vTop)
                     else:
-                        self.newVal(vTop,vDown)
+                        self.newVal(vTop, vDown)
 
     def newVal(self,val,nVal):
         """"""
@@ -132,6 +135,14 @@ class labyrinthe:
                 else:
                     t.append("□")
             print("".join(t))
+
+    def wallaround(self, x, y):
+        list = []
+        dic = self.get_cell(x, y).cellAdj(self.width, self.height)
+        for i in dic:
+            if self.get_cell(**dic[i]).wall:
+                list.append(i)
+        return list
 
 
 if __name__ == "__main__":
