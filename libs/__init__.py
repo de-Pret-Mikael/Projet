6f09ee0731b4598690cbf7f7cb125a2dfb461985
cyrilGrandjean <64021617+cyrilGrandjean@users.noreply.github.com>
2020-11-01 19:01:50 +0100

    update import

---
+++ libs/__init__.py
@@ -0,0 +1,2 @@
+from .hero import *
+from .labyrinthe import *
---
+++ main.py
@@ -0,0 +1,8 @@
+from libs.labyrinthe import *
+from libs.hero import *
+
+if __name__ == "__main__":
+    l =labyrinthe(3,6)
+    png = hero()
+    l.show()
+    print(l,png)
