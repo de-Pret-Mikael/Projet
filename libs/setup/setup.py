dd7f4f8cb66de3c8129caae8ddbd17af71e74add
Mikaël de Pret <he201775@students.ephec.be>
2020-11-25 16:37:17 +0100

    ajout setup

---
+++ libs/setup/setup.py
@@ -0,0 +1,20 @@
+import sys, os
+
+
+os.environ['TCL_LIBRARY'] = 'c:/python38/tcl/tcl8.6'
+os.environ['TK_LIBRARY'] = 'c:/python38/tcl/tk8.6'
+from cx_Freeze import setup, Executable
+
+buildOptions =  {"packages": ["os"]}
+base = None
+if sys.platform == 'Win32':
+    base = 'Win32Gui'
+
+print(sys.platform)
+
+setup(
+      name='ajout',
+      version = '1.0',
+      description = 'Package pour pouvoir jouer',
+      options = {'build_exe': buildOptions},
+      executables = [Executable('TkApp.py', base=base)])
